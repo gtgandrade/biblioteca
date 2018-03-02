98ca2eb72f87aed6292ea07ab51ec48fef7344f3
gtgandrade <gtgandrade@gmail.com>
2018-03-02 01:54:29 -0300

    Begin Project

---
+++ src/main/java/biblioteca/maven/modelo/Usuario.java
@@ -0,0 +1,22 @@
+package biblioteca.maven.modelo;
+
+public class Usuario {
+
+	private String nome;
+
+	private String matricula;
+
+	public Usuario(String nome, String matricula) {
+		super();
+		this.nome = nome;
+		this.matricula = matricula;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public String getMatricula() {
+		return matricula;
+	}
+}
